6b55f0ae2247bee8401f98f1339324a416c98890
Dileep Raloti <56366180+ddr-dileep@users.noreply.github.com>
2024-08-23 14:49:18 +0530

    add types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express, { Request, Response } from "express";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send('Hello, TypeScript with Express!');
 });
 
