8e584a1e9df450a5f0444190e1cd25317de3c7c6
Dileep Raloti <56366180+ddr-dileep@users.noreply.github.com>
2024-08-30 13:19:28 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from "express";
 import { config } from "dotenv";
+import { databaseConnection } from "./configs/db-config";
 
 // env and app configuration
 config();
+databaseConnection();
 const app = express();
 const port: string = process.env.APPLICATION_PROT || "8000";
 
 app.get("/", (req: Request, res: Response) => {
-  res.send('Hello, TypeScript with Express!');
+  res.send("Hello, TypeScript with Express!");
 });
 
 app.listen(port, () => {
