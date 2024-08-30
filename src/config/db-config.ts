c1f3c2d24c6ad792017adb0c7a5da9c70c83fcb7
Dileep Raloti <56366180+ddr-dileep@users.noreply.github.com>
2024-08-30 13:18:01 +0530

    Update db-config.ts

--- src/config/db-config.ts
+++ src/config/db-config.ts
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export const databaseConnection = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI!);
+    await mongoose.connect(process.env.MONGODB_URI!, {
+      dbName: "TEMP", // change this to your database name
+    });
     console.log("Connected to MongoDB!");
   } catch (error) {
     console.error("Database connection error");
